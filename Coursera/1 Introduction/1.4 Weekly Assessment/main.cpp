c029048126cd543a202df398fbdaa4a5a6532bb1
R. N. Savinelli <rns18@student.london.ac.uk>
2024-03-01 08:25:43 -0300

    add week 1 assignment

---
+++ Coursera/1 Introduction/1.4 Weekly Assessment/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int main(void) {
+    std::cout << ":: Merkelrex" << std::endl;
+
+    while(true) {
+        std::cout << std::endl;
+        std::cout << "1: Print help" << std::endl;
+        std::cout << "2: Print exchange stats" << std::endl;
+        std::cout << "3: Place an ask" << std::endl;
+        std::cout << "4: Place a bid" << std::endl;
+        std::cout << "5: Print wallet" << std::endl;
+        std::cout << "6: Continue" << std::endl;
+
+        int userOption = 0;
+        std::cout << std::endl << ":: Type in 1-6: ";
+        std::cin >> userOption;
+
+        std::cout << std::endl << ":: Your selection: " << userOption << std::endl << std::endl;
+
+        switch(userOption) {
+            case(1):
+                std::cout << "Help - choose options from the menu" << std::endl;
+                std::cout << "and follow the on screen instructions." << std::endl;
+                break;
+            case(2):
+                std::cout << "Print exchange stats - Market looks good" << std::endl;
+                break;
+            case(3):
+                std::cout << "Place an ask - Enter the amount" << std::endl;
+                break;
+            case(4):
+                std::cout << "Place an bid - Enter the amount" << std::endl;
+                break;
+            case(5):
+                std::cout << "Print wallet - Your wallet is empty" << std::endl;
+                break;
+            case(6):
+                std::cout << "Continue - Going to the next time frame" << std::endl;
+                break;
+            default:
+                std::cout << "Error - Invalid selection" << std::endl;
+                break;
+        }
+    }
+
+    return 0;
+}
